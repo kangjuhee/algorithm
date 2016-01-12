0f16b7565fc9ebfdef7021f2c8fb26ee6f03e850
younghookim <younghoo88@gmail.com>
2016-01-12 23:25:17 +0900

    18강 조세푸스 문제 풀이

--- src/chap18/Q1younghoo.java
+++ src/chap18/Q1younghoo.java
@@ -1,12 +1,41 @@
 package chap18;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Q1younghoo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//test 
-		//test3
-		//test2
+		
+		Scanner sc = new Scanner(System.in);
+		int numOfCase = sc.nextInt();
+		
+		for (int t = 0; t < numOfCase; t++) {
+			
+			int numOfPpl = sc.nextInt();
+			int skip = sc.nextInt();
+			// 병사 list 초기화
+			List<Integer> list = new ArrayList<Integer>();
+			for (int i = 0; i < numOfPpl; i++) {
+				list.add(i+1);
+			}
+			// 실제 알고리즘 구현부분
+			int idx = 0;
+			
+			while (list.size() > 2) {
+				int size = list.size(); // size backup
+				list.remove(idx); // idx에 해당하는 병사 제거
+				idx = (idx + skip - 1) % (size - 1);
+			}
+			
+			// 결과값 출력
+			Iterator<Integer> iterator = list.iterator();
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
+		}
+		sc.close();
 	}
-
 }
