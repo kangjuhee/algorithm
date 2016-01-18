5bf8eeb38ef76b46b2b7b18a0de150699e046ab6
younghookim <younghoo88@gmail.com>
2016-01-19 00:50:51 +0900

    chap19/Q1juhee indentation 수정 및 chap21 Q1 제출(코드 분석 필요)

--- src/chap19/Q1juhee.java
+++ src/chap19/Q1juhee.java
@@ -12,67 +12,68 @@ public class Q1juhee {
 		int times;
 		times = in.read() - 48; // 아스키 값으로 받는다.
 		
-		bracket[] input = new bracket[times];
-		boolean[] status = new boolean[times];	
-		for(int x = 0; x < times; x ++) { 	
-			bracket b = new bracket();
+		Bracket[] input = new Bracket[times];
+		boolean[] status = new boolean[times];
+		for(int x = 0; x < times; x ++) {
+			Bracket b = new Bracket();
 			boolean bl = true;
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			String str = reader.readLine();
-
-			for(int i = 0; i < str.length(); i++){
+			
+			for(int i = 0; i < str.length(); i++) {
 				char c = str.charAt(i);
 				if(i % 2 == 0) {//TODO 공백체크 하는 걸로 바꿔야댐 바보 ㅉㅉ
 					b.brackets.add(Character.toString(c));
 					System.out.println("here : "  + c);
-					}
+				}
 			}
 
 			List<String> addme = new ArrayList<String>();
 			int i = 0;
 			String val;
-			int lastIndex; //입력받은 input의 last index			
+			int lastIndex; //입력받은 input의 last index
 
-			for(int a = 0; a < b.brackets.size(); a++) { 
+			for(int a = 0; a < b.brackets.size(); a++) {
 				// (왼쪽일 경우)test에 입력값을 넣고 (왼쪽일 경우)
 				// (오른쪽일 경우) 입력값을 pop 한다 -- > need to check status
 				val = b.brackets.get(i++); //input의 value를 순차적으로 get
 				System.out.println("VAL : " +val);
-				if(val.equals("{") || val.equals("(") || val.equals("[")){ //왼쪽의 값이면 addme에 add
+				if(val.equals("{") || val.equals("(") || val.equals("[")) { //왼쪽의 값이면 addme에 add
 					addme.add(val);	
-				}
-				else {  // 값이 오른쪽 문자일경우 
+				} else {  // 값이 오른쪽 문자일경우 
 					String lastVal; //입력받은 input의 last value	
 					lastIndex = addme.size()-1;
 					lastVal = addme.get(lastIndex); // addme에 받은 마지막 원소(왼쪽 기호)를 꺼낸다		
 					if(val.equals(")")) {
-						if(lastVal.equals("(")) { System.out.println("ㅈ겹다:");					
-							addme.remove(lastIndex);	status[times - 1]= true;
-							}
-						else {
-							bl = false;	
+						if(lastVal.equals("(")) { System.out.println("ㅈ겹다:"); // ㅋㅋ					
+							addme.remove(lastIndex);	
+							status[times - 1] = true;
+						} else {
+							bl = false;
 							input[times - 1] = b;
-							status[times - 1]= bl;
+							status[times - 1] = bl;
 							break;
-						}	
+						}
 					} else if (val.equals("}")) {
-						if(lastVal.equals("{")) {status[times - 1]= true;
+						if(lastVal.equals("{")) {
+						    status[times - 1] = true;
 							addme.remove(lastIndex);}
 						else {
 							bl = false; 
 							input[times - 1] = b; 
-							status[times - 1]=bl; 
+							status[times - 1] = bl; 
 							break;
-							}
+						}
 					} else if (val.equals("]")) {
-						if(lastVal.equals("[")) {status[times - 1]= true;
-							addme.remove(lastIndex);}
-						else {
+						if(lastVal.equals("[")) {
+						    status[times - 1] = true;
+							addme.remove(lastIndex);
+						} else {
 							bl = false; 
 							input[times - 1] = b; 
-							status[times - 1]=bl;
+							status[times - 1] = bl;
 							break;
-							}
+						}
 					}
 				System.out.println(x + " : " +  status[0]);
 				if(lastIndex == 0) break;
@@ -81,14 +82,14 @@ public class Q1juhee {
 			} // end of for
 			
 		}
-		for ( int r = 0; r < times; r++ ) {
+		for(int r = 0; r < times; r++) {
 			System.out.println(status[r]);
 		}
 	} // end of class 
 	
-	public static class bracket {
-		public  List<String> brackets = new ArrayList<String>();
-//		public  boolean status = true;
+	public static class Bracket { // (수정)클래스의 첫 문자는 대문자로
+		public List<String> brackets = new ArrayList<String>();
+//		public boolean status = true;
 	}
 }
 
---
+++ src/chap21/Q1younghoo.java
@@ -0,0 +1,92 @@
+package chap21;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1younghoo {
+
+    /**
+     * int형 array를 first index부터 last index까지 자르는 함수이다.
+     * @param arr
+     * @param first
+     * @param last
+     * @return
+     */
+    public static int[] slice(int[] arr, int first, int last) {
+
+        return Arrays.copyOfRange(arr, first, last + 1);
+    }
+
+    public static int find(int[] arr, int target) {
+        int idx = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                idx = i;
+                break;
+            }
+        }
+        return idx;
+    }
+
+    public static void traversal(int[] preOrder, int[] inOrder) {
+
+        int parent = preOrder[0];
+        preOrder = slice(preOrder, 1, preOrder.length-1);
+
+        int rootIndexInInOrder = find(inOrder, parent);
+
+        if (inOrder.length == 1) {
+            System.out.println(inOrder[0]);
+            return;
+        } else if (inOrder.length == 0) {
+            return;
+        }
+
+        if (rootIndexInInOrder == -1) {
+            traversal(preOrder, inOrder);
+            return;
+        }
+
+        int[] leftInOrder = slice(inOrder, 0, rootIndexInInOrder - 1);
+        int[] rightInOrder = slice(inOrder, rootIndexInInOrder + 1, inOrder.length - 1);
+
+        traversal(preOrder, leftInOrder);
+        traversal(preOrder, rightInOrder);
+        System.out.println(parent);
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(new FileInputStream("./input.txt"));
+        System.out.println("전체 테스트 케이스 개수를 입력해주세요 : ");
+        int numofCase = sc.nextInt();
+
+        /**
+         * preOrder, inOrder list를 초기에 arrayList로 하려고 하였으나
+         * slice를 간단히 구현해보기 위해 그냥 array로 선언하였다.
+         */
+        for (int i = 0; i < numofCase; i++) {
+            System.out.println("트리의 전체 노드 개수를 입력해주세요 : ");
+            int numOfNode = sc.nextInt(); // 트리의 전체 노드 개수
+            System.out.println("트리의 전체 노드 개수 : " + numOfNode);
+            int[] preOrder = new int[numOfNode];
+            int[] inOrder = new int[numOfNode];
+
+            System.out.println("전위순회 순서를 입력해주세요 : ");
+            for (int j = 0; j < numOfNode; j++) {
+                preOrder[j] = sc.nextInt();
+            }
+
+            System.out.println("중위순회 순서를 입력해주세요 : ");
+            for (int j = 0; j < numOfNode; j++) {
+                inOrder[j] = sc.nextInt();
+            }
+
+            // 알고리즘 시작
+            traversal(preOrder, inOrder);
+        }
+    }
+}
